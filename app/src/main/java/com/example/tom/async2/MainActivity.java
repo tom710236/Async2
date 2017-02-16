da56bf0743c0117955fd30da7dd30f3ddb08b5a4
TOM <tom710236@gmail.com>
2017-02-16 17:41:29 +0800

    JobTask3

--- app/src/main/java/com/example/tom/async2/MainActivity.java
+++ app/src/main/java/com/example/tom/async2/MainActivity.java
@@ -1,88 +1,26 @@
 package com.example.tom.async2;
 
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //在畫面上按go1執行Job1Task
     public void go1(View v){
         textView = (TextView)findViewById(R.id.info);
             new Job1Task().execute();
-
-
-
     }
-    //產生Job2Task物件並執行execute方法 將秒數3傳入
-    public void go2(View v) {
-        new Job2Task().execute(3);
-    }
-
-    public void go3(View v) {
-
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
     //工作五秒後 在TextView顯示DONE
     class Job1Task extends AsyncTask<Void, Void, Void> {
 
@@ -94,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
         @Override
         protected Void doInBackground(Void... params) {
             try {
@@ -104,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+    }
+    //產生Job2Task物件並執行execute方法 將秒數3傳入
+    public void go2(View v) {
+        new Job2Task().execute(3);
     }
     //工作三秒後 在TextView顯示DONE
     class Job2Task extends AsyncTask <Integer,Void,Void>{
@@ -124,6 +65,42 @@ public class MainActivity extends AppCompatActivity {
                 Thread.sleep(params[0]*1000);
             }catch (InterruptedException e){
                 e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    //產生Job3Task物件並執行execute方法 將秒數6傳入
+    public void go3(View v) {
+        new Job3Task().execute(6);
+    }
+    class Job3Task extends AsyncTask<Integer,Integer,Void>{
+        TextView info;
+        public Job3Task(){
+            info = (TextView)findViewById(R.id.info);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            info.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            info.setText("DONE");
+        }
+        //使用for迴圈 由傳入值遞增到6
+        @Override
+        protected Void doInBackground(Integer... params) {
+            for (int i=0;params[0]>i;i++){
+                publishProgress(i);
+                try{
+                    //每次回圈執行停頓一秒
+                    Thread.sleep(1000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
             }
             return null;
         }
