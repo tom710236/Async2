27885bbbb58cfb44741d90ca35d2cdb2af17a503
TOM <tom710236@gmail.com>
2017-02-16 15:52:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tom/async2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tom.async2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void go1 (View v){
+
+    }
+    public void go2 (View v){
+
+    }
+    public void go3(View v){
+        
+    }
+}
